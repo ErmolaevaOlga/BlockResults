8c1b3d93ef52e40795d0d25d9ea69b031e1a5d72
Olga Ermolaeva <photo-deti@mail.ru>
2023-03-10 18:27:19 +0700

    Добавлен текст программы

--- task/Program.cs
+++ task/Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿Console.Clear();
+Console.WriteLine("Введите строки через запятую:");
+string numbers = Console.ReadLine();
+
+string[] massiv = GetArrayFromString(numbers);
+
+string[] result = new string[CountElement(massiv)];
+int j = 0;
+for (int i = 0; i < massiv.GetLength(0); i++)
+    {
+        if (massiv[i].Length <= 3)
+        {
+            result[j] = massiv[i];
+            j++;
+        }
+                
+    }
+
+Console.WriteLine($"[{String.Join(", ", result)}]");
+
+// Перевод введенной строки в массив
+string[] GetArrayFromString(string text)
+{
+    string[] result = text.Split( ',', StringSplitOptions.RemoveEmptyEntries);
+    
+    return result;
+}
+
+// Подсчет количества строк длиной <= 3
+int CountElement (string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+                
+    }
+    return count;
+}
+
+
